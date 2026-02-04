244f3af7d4e3c216228097055aa7a64a88f5a5f9
abhijitdrag <hackerbolte012@gmail.com>
2026-02-04 14:11:22 +0530

    half prcoess

---
+++ CRYPTOPLACE/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import React, { useContext } from 'react'
+import './Navbar.css' 
+import logo from '../../assets/logo.png'
+import arrow_icon from '../../assets/arrow_icon.png'
+import { CoinContext } from '../../context/CoinContext'
+
+const Navbar = () => {
+
+  const {setCurrency} = useContext(CoinContext)
+
+  const currencyHander = (event)=>{
+    switch (event.target.value){
+      case "usd":{
+        setCurrency({name : "usd", Symbol: "$"});
+        break;
+      }
+      case "eur":{
+        setCurrency({name : "eur", Symbol: "€"});
+        break;
+      }
+      case "inr":{
+        setCurrency({name : "inr", Symbol: "₹"});
+        break;
+      }
+      default:{
+        setCurrency({name : "usd", Symbol: "$"});
+        break;
+      }
+    }
+  }
+
+  return (
+    <div className='navbar'>
+      <img src={logo} alt="" className='logo' />
+      <ul>
+        <li>Home</li>
+        <li>Features</li>
+        <li>Pricing</li>
+        <li>Blog</li>
+      </ul>
+      <div className="nav-right">
+        <select onChange={currencyHander}>
+            <option value="usd">USD</option>
+            <option value="eur">EUR</option>
+            <option value="inr">INR</option>
+        </select>
+        <button>Sign up <img src={arrow_icon} alt="" /></button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ CRYPTOPLACE/src/pages/Home/Home.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useContext, useEffect } from 'react'
+import { CoinContext } from '../../context/CoinContext'
+import './Home.css'
+
+const Home = () => {
+
+  const {allCoin, currency} = useContext(CoinContext);
+  const [displayCoin, setDisplayCoin] = useState([]);
+
+  useEffect(()=>{
+    setDisplayCoin(allCoin);
+  }, [allCoin])
+
+  return (
+    <div>
+      <div className='home'>
+        <div className='hero'>
+          <h1>Largest <br/> Crypto Marketplace</h1>
+          <p>Welcome to the world's largest cryptocurrency
+            marketplace. Sign up to explore more about cryptos.
+          </p>
+          <form>
+            <input type="text" placeholder='Search crypto..'/>
+            <button type='submit'>Search</button>
+          </form>
+        </div>
+        <div className='crypto-table'>
+          <div className="table-layout">
+            <p>#</p>
+            <p>Coins</p>
+            <p>Price</p>
+            <p style={{textAlign:"center"}}>24H Change</p>
+            <p className='market-cap'>Market Cap</p>
+          </div>
+          {
+            displayCoin.slice(0,10).map((item, index)=>(
+              <div className="table-layout" key={index}>
+                <p>{item.market_cap_rank}</p>
+                <div>
+                  <img src={item.image} alt="" />
+                  <p>{item.name + " - " + item.symbol}</p>
+                </div>
+                <p>{currency.symbol} {item.current_price}</p>
+              </div>  
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
